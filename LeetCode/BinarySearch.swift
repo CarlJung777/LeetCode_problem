be7b21a932a65dc8a8d19100c19bcad61924a23a
CarlJung777 <125782812+CarlJung777@users.noreply.github.com>
2024-09-30 06:23:58 +0800

    BinarySearch

---
+++ LeetCode/BinarySearch.swift
@@ -0,0 +1,10 @@
+class Sloution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right =nums.count - 1
+        while left <= right {
+            let mid = left + (right - left) / 2
+
+        }
+    }
+}
